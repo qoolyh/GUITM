a0b96d8289e7b279234680f28e090a28a01d5171
qoolyh <qoolyh@gmail.com>
2020-12-25 10:10:54 +0800

    update ipt

--- InputGenerator.py
+++ InputGenerator.py
@@ -114,7 +114,7 @@ def getAnswer(ori):
                 else:
                     oneact = {act:[e['action'][1]]}
                     res.update(oneact)
-    return res
+    return res1
 #
 #
 # print(getAnswer('data/a3_b31/a31.json'))
